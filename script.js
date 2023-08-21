b671d892958ec8629e409a3042c13a83470d2da1
Nayeem Ahmed <134209701+NayeemAhmed23@users.noreply.github.com>
2023-08-21 23:36:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+const form = document.getElementById("form1");
+	function getFormvalue(){
+		let fullName = form["fname"].value + " " + form["lname"].value;
+		alert[fullName]
+	}
 }
